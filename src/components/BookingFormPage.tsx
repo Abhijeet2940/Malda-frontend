ff99c448d991c0ede9187e41849f217c7950ed8f
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2026-06-11 14:17:00 +0530

    Update BookingFormPage recaptcha site key

--- src/components/BookingFormPage.tsx
+++ src/components/BookingFormPage.tsx
@@ -1227,7 +1227,7 @@ export const BookingFormPage: React.FC = () => {
                     Verification <span className="required">*</span>
                   </label>
                   <ReCAPTCHA
-                    sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                    sitekey="6LfIu-csAAAAAEKFpBO6cMzmsdGLeQ_BNQtc88zQ"
                     onChange={(value: string | null) => setFormData({ ...formData, captcha: value ?? "" })}
                   />
                 </div>
